164ee075d0a04940bc8794adec9b24d4d8b7e534
luviabit <275095550+luviabit@users.noreply.github.com>
2026-04-28 17:21:20 +1200

    added initial project files

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("clickBtn");
+
+    button.addEventListener("click", () => {
+        alert("meow :3");
+    });
+});
